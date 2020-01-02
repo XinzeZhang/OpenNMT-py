9c10e4144169a41cdea6a95ee45dfcd163185399
xinzezhang <xinze@hust.edu.cn>
2020-01-03 00:21:56 +0800

    update sharebpe

--- sh_sharebpe.sh
+++ sh_sharebpe.sh
@@ -1,14 +1,17 @@
 data_path=$PWD'/trial/ldc/'
-run_path=$PWD'/trial/ldc_en_zh/'
+run_path=$PWD'/trial/ldc_exp/'
 valid_folder='nist03/nist03.'
 
-en_path=$data_path'train.cn-en.en'
-en_bpt_path=$run_path'train.en.bpe'
+# for l in en zh; do for f in $data_path/*.$l; do if [[ "$f" != *"test"* ]]; then sed -i "$ d" $f; fi;  done; done
+# for l in en zh; do for f in $data_path/*.$l; do perl tools/tokenizer.perl -a -no-escape -l $l -q  < $f > $f.atok; done; done
+
+en_path=$data_path'train.cn-en.en.atok'
+en_bpe_path=$run_path'train.en.bpe'
 
 en_valid_path=$data_path$valid_folder'en0'
 en_valid_bpe_path=$run_path$valid_folder'en0.bpe'
 
-zh_path=$data_path'train.cn-en.zh'
+zh_path=$data_path'train.cn-en.zh.atok'
 zh_bpe_path=$run_path'train.zh.bpe'
 
 zh_valid_path=$data_path$valid_folder'cn'
@@ -20,16 +23,17 @@ zh_bpe_vocab=$run_path'train.zh.bpe.vocab'
 
 # pip install subword-nmt
 
-cat zh_path $en_path | subword-nmt learn-bpe -s 60000 -o $share_codes
-subword-nmt apply-bpe -c $share_codes < $zh_path | subword-nmt get-vocab > $zh_bpe_vocab
-subword-nmt apply-bpe -c $share_codes < $en_path | subword-nmt get-vocab > $en_bpe_vocab
 
-# subword-nmt learn-joint-bpe-and-vocab --input {train_file}.L1 {train_file}.L2 -s {num_operations} -o $share_codes --write-vocabulary {vocab_file}.L1 {vocab_file}.L2
+# cat $zh_path $en_path | subword-nmt learn-bpe -s 60000 -o $share_codes
+# subword-nmt apply-bpe -c $share_codes < $zh_path | subword-nmt get-vocab > $zh_bpe_vocab
+# subword-nmt apply-bpe -c $share_codes < $en_path | subword-nmt get-vocab > $en_bpe_vocab
+
+subword-nmt learn-joint-bpe-and-vocab --input $zh_path $en_path -s 60000 -o $share_codes --write-vocabulary $zh_bpe_vocab $en_bpe_vocab
+
+subword-nmt apply-bpe -c $share_codes --vocabulary $en_bpe_vocab --vocabulary-threshold 50 < $en_path > $en_bpe_path
+
+subword-nmt apply-bpe -c $share_codes --vocabulary $en_bpe_vocab --vocabulary-threshold 50 < $en_valid_path > $en_valid_bpe_path
 
 subword-nmt apply-bpe -c $share_codes --vocabulary $zh_bpe_vocab --vocabulary-threshold 50 < $zh_path > $zh_bpe_path
 
 subword-nmt apply-bpe -c $share_codes --vocabulary $zh_bpe_vocab --vocabulary-threshold 50 < $zh_valid_path > $zh_valid_bpe_path
-
-subword-nmt apply-bpe -c $share_codes --vocabulary $en_bpe_vocab --vocabulary-threshold 50 < $en_path > $en_bpe_vocab
-
-subword-nmt apply-bpe -c $share_codes --vocabulary {vocab_file}.L1 --vocabulary-threshold 50 < $en_valid_path > $en_valid_bpe_path
